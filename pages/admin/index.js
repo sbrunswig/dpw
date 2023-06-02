d185a563919eee1d8c2b465bad03862ead194dc8
Casey Woodman <caseytwoodman@gmail.com>
2023-06-02 14:45:02 -0500

    setup text area on admin, accepts JSON, parses to look right, strings it ready to update on Mongo

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,6 +1,38 @@
 import Head from "next/head";
+import { useReducer } from "react";
+
+const formReducer = (state, event) => {
+  const uglyJson = event.target.value;
+  const obj = JSON.parse(uglyJson);
+  const prettyJson = JSON.stringify(obj, undefined, 4);
+  return {
+    ...state,
+    [event.target.name]: prettyJson,
+  };
+};
 
 export default function ADmin() {
+  const [formData, setFormData] = useReducer(formReducer, {});
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(formData);
+    console.log(formData.english);
+    console.log(JSON.parse(formData.english));
+  };
+
+  const prettyJson = (event) => {
+    const uglyJson = document.getElementById("english").value;
+    const obj = JSON.parse(uglyJson);
+    const prettyJson = JSON.stringify(obj, undefined, 4);
+    document.getElementById("english").value = prettyJson;
+    console.log(prettyJson);
+    const stringed = JSON.stringify(prettyJson);
+    console.log("string", stringed);
+    const parsed = JSON.parse(stringed);
+    console.log("parsed", parsed);
+  };
+
   return (
     <>
       <Head>
@@ -11,6 +43,14 @@ export default function ADmin() {
       </Head>
 
       <h1 className="md:text-center">admin</h1>
+      <button onClick={handleSubmit}>Submit</button>
+      <button onClick={prettyJson}>Pretty</button>
+      <form className="flex flex-col">
+        <label htmlFor="english" className="font-bold text-primary">
+          English
+        </label>
+        <textarea id="english" name="english" rows="50" cols="50" onChange={setFormData}></textarea>
+      </form>
     </>
   );
 }
